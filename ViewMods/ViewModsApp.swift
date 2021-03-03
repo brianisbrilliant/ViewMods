88be9bdc24c3eae4677ef4889923c483ee9b0ad1
Brian Foster <bfoster6@butlercc.edu>
2021-03-03 09:02:20 -0600

    Initial Commit

---
+++ ViewMods/ViewModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModsApp.swift
+//  ViewMods
+//
+//  Created by Professor Foster on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
